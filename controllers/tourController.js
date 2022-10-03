d288c5fae6bc95681de90ee1e13df9b921cb1120
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-10-03 21:56:21 +0300

    Field limit

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -35,6 +35,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    /////FIELD LIMIT QUERY////
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v"); //Exclude the __v field
+    }
+
     //////EXECUTE QUERY////
     const tours = await query;
 
