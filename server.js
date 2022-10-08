d93e4bee5dbdbbb87c10a5f5f1e03428a774fb46
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-10-08 23:47:56 +0300

    Handle other errors

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! SHUTING DOWN...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
@@ -22,6 +29,14 @@ mongoose
 
 //START SERVER
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on ${port}...`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! SHUTING DOWN...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
