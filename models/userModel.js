0a69030a2515cabda5193703133dbe015f0075a1
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-10-09 22:20:55 +0300

    user model

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please tell us your name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide us your email"],
+    unique: true,
+    lowercase: true,
+    validate: [validator.isEmail, "Please provide us a valid email"],
+  },
+  photo: {
+    type: String,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide us your password"],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: Number,
+    required: [true, "Please confirm your password"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
