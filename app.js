7e232148aa7ed44eaa91e51bca1c562bc543f5e1
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-10-18 22:28:19 +0300

    Add security methods

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,18 +13,55 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// MIDDLEWARES: These middleware apply to all routes below
+///////////////////////////////////////////
+// GLOBAL MIDDLEWARES: These middleware apply to all routes below
+//To set security headers
+app.use(helmet());
+
+//To log development
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.use(express.json());
+//To limit the requests coming from 1 IP => block these requests
+const limiter = rateLimit({
+  max: 100, //number of max requests
+  windowMs: 60 * 60 * 1000, //Milisecond time window (1hour)
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 
+//To read data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+//To sanitize data against NoSQL query injection
+app.use(mongoSanitize());
+
+//To sanitize data against XSS attacks
+app.use(xss());
+
+//To prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      //Except for these parameters
+      "duration",
+      "ratingsQuantity",
+      "ratingsAverage",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+//To test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+///////////////////////////////////////////
 //ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
