b140f87072ed8c90c16f21738f6fea1b352d2566
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-10-19 19:47:23 +0300

    Embed guides

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
 
+const User = require("./userModel");
+
 const tourSchema = new mongoose.Schema(
   {
     name: {
@@ -63,6 +65,32 @@ const tourSchema = new mongoose.Schema(
     },
     startDates: [Date],
     secretTour: { type: Boolean, default: false },
+    startLocation: {
+      //GeoJSON
+      type: {
+        type: "String",
+        default: "Point",
+        enum: ["Point"],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      //Embeded documents
+      {
+        type: {
+          type: "String",
+          default: "Point",
+          enum: ["Point"],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: Array,
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } } //Whenever data outputs into JSON or object, virtuals will be shown
 );
@@ -76,6 +104,12 @@ tourSchema.virtual("durationWeeks").get(function () {
 ////////DOCUMENT MIDDLEWARE: runs before .save() and .create()
 tourSchema.pre("save", function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre("save", async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
