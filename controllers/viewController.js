b7aa24056b02816c587d3844c6096ca2d382c1d8
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-10-31 15:05:26 +0200

    tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -6,11 +6,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
 
   //2. Build template
-
   //3. Render template
   res.status(200).render("overview", { title: "All Tours", tours });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render("tour", { title: "The Forest Hiker" });
-};
+exports.getTour = catchAsync(async (req, res) => {
+  //1. Get requested tour data
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: "reviews",
+    fields: "review rating user",
+  });
+
+  //2. Build template
+  //3. Render template
+  res.status(200).render("tour", { title: "The Forest Hiker", tour });
+});
