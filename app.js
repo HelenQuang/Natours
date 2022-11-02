702f40209fdc7878a64534f6957874e7777a5910
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-11-02 23:38:36 +0200

    Log in user

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const helmet = require("helmet");
 const mongoSanitize = require("express-mongo-sanitize");
 const xss = require("xss-clean");
 const hpp = require("hpp");
+const cookieParser = require("cookie-parser");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -43,6 +44,9 @@ app.use("/api", limiter);
 //To read data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 
+//To read data from browser cookie
+app.use(cookieParser());
+
 //To sanitize data against NoSQL query injection
 app.use(mongoSanitize());
 
@@ -67,6 +71,7 @@ app.use(
 //To test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies);
   next();
 });
 
--- controllers/viewController.js
+++ controllers/viewController.js
@@ -10,7 +10,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   res.status(200).render("overview", { title: "All Tours", tours });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   //1. Get requested tour data
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: "reviews",
@@ -22,14 +22,14 @@ exports.getTour = catchAsync(async (req, res) => {
   res.status(200).render("tour", { title: `${tour.name} tour`, tour });
 });
 
-exports.getLoginForm = catchAsync(async (req, res) => {
+exports.getLoginForm = (req, res) => {
   res.status(200).render("login", {
     title: "Log into your account",
   });
-});
+};
 
-exports.getSignupForm = catchAsync(async (req, res) => {
+exports.getSignupForm = (req, res) => {
   res.status(200).render("signup", {
     title: "Sign up to new account",
   });
-});
+};
